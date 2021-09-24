17b3478978c1edab9e80d63446c947e62f60a9d4
Kryle <79478161+Kryle@users.noreply.github.com>
2021-09-24 16:52:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+var i = 0;
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 38) {
+		setTimeout(konami2(),200)
+    }
+}, true);
+
+function konami2(){
+	document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 38) {
+		setTimeout(konami3(),200)
+    }
+}, true);
+}
+
+function konami3(){
+	document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 40) {
+		setTimeout(konami4(),200)
+    }
+}, true);
+}
+
+function konami4(){
+	document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 40) {
+		setTimeout(konami5(),200)
+    }
+}, true);
+}
+
+function konami5(){
+	document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 37) {
+		setTimeout(konami6(),200)
+    }
+}, true);
+}
+function konami6(){
+	document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 39) {
+		setTimeout(konami7(),200)
+    }
+}, true);
+}
+function konami7(){
+	document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 37) {
+		setTimeout(konami8(),200)
+    }
+}, true);
+}
+function konami8(){
+	document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 39) {
+		setTimeout(konami9(),200)
+    }
+}, true);
+}
+function konami9(){
+	document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 66) {
+		setTimeout(konami10(),200)
+    }
+}, true);
+}
+function konami10(){
+	document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 65 && i == 0) {
+		i = 1;
+		alert('OUI');
+    }
+}, true);
+}
